871ec484d834740a60fed263d75be64e2ebd5743
nloranni <nicolas.lorant@vesperiagroup.com>
2016-04-12 09:59:17 +0000

    MULTISITE-9591 Fix Taxonomy tranlated

--- lib/features/farnet_taxonomy/includes/farnet_taxonomy_publication_channels.php
+++ lib/features/farnet_taxonomy/includes/farnet_taxonomy_publication_channels.php
@@ -9,10 +9,38 @@
  */
 function farnet_taxonomy_publication_channels() {
   $publication_channels_array = array(
-    'Channel1' => t('Farnet Publication Channel1'),
-    'Channel2' => t('Farnet Publication Channel2'),
-    'Channel3' => t('Farnet Publication Channel3'),
-    'Channel4' => t('Farnet Publication Channel4'),
+    'Channel1' => (object) array(
+      'name' => t('Farnet Type Channel1 EN'),
+      'language' => 'en',
+      'translation' => array(
+        'fr' => t('Farnet Type Channel1 FR'),
+        'it' => t('Farnet Type Channel1 IT'),
+      ),
+    ),
+    'Channel2' => (object) array(
+      'name' => t('Farnet Type Channel2 EN'),
+      'language' => 'en',
+      'translation' => array(
+        'fr' => t('Farnet Type Channel2 FR'),
+        'it' => t('Farnet Type Channel2 IT'),
+      ),
+    ),
+    'Channel3' => (object) array(
+      'name' => t('Farnet Type Channel3 EN'),
+      'language' => 'en',
+      'translation' => array(
+        'fr' => t('Farnet Type Channel3 FR'),
+        'it' => t('Farnet Type Channel3 IT'),
+      ),
+    ),
+    'Channel4' => (object) array(
+      'name' => t('Farnet Type Channel4 EN'),
+      'language' => 'en',
+      'translation' => array(
+        'fr' => t('Farnet Type Channel4 FR'),
+        'it' => t('Farnet Type Channel4 IT'),
+      ),
+    ),
   );
   return $publication_channels_array;
 }
--- lib/features/farnet_taxonomy/includes/farnet_taxonomy_type_publications.php
+++ lib/features/farnet_taxonomy/includes/farnet_taxonomy_type_publications.php
@@ -9,10 +9,38 @@
  */
 function farnet_taxonomy_type_publications() {
   $type_publications_array = array(
-    'Publication1' => t('Farnet Type Publication1'),
-    'Publication2' => t('Farnet Type Publication2'),
-    'Publication3' => t('Farnet Type Publication3'),
-    'Publication4' => t('Farnet Type Publication4'),
+    'Publication1' => (object) array(
+      'name' => t('Farnet Type Publication1 EN'),
+      'language' => 'en',
+      'translation' => array(
+        'fr' => t('Farnet Type Publication1 FR'),
+        'it' => t('Farnet Type Publication1 IT'),
+      ),
+    ),
+    'Publication2' => (object) array(
+      'name' => t('Farnet Type Publication2 EN'),
+      'language' => 'en',
+      'translation' => array(
+        'fr' => t('Farnet Type Publication2 FR'),
+        'it' => t('Farnet Type Publication2 IT'),
+      ),
+    ),
+    'Publication3' => (object) array(
+      'name' => t('Farnet Type Publication3 EN'),
+      'language' => 'en',
+      'translation' => array(
+        'fr' => t('Farnet Type Publication3 FR'),
+        'it' => t('Farnet Type Publication3 IT'),
+      ),
+    ),
+    'Publication4' => (object) array(
+      'name' => t('Farnet Type Publication4 EN'),
+      'language' => 'en',
+      'translation' => array(
+        'fr' => t('Farnet Type Publication4 FR'),
+        'it' => t('Farnet Type Publication4 IT'),
+      ),
+    ),
   );
   return $type_publications_array;
 }
