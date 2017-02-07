fea0c014df190b1767f9bcb61260f5ab44e6a032
LIBERT Thomas <Thomas.LIBERT@ext.ec.europa.eu>
2017-02-07 10:34:38 +0100

    cs

--- lib/themes/farnet/tpl/views/views-exposed-form.tpl.php
+++ lib/themes/farnet/tpl/views/views-exposed-form.tpl.php
@@ -10,8 +10,8 @@
  * - $widget->operator: The operator for the widget. May be optional.
  * - $widget->widget: The widget itself.
  * - $sort_by: The select box to sort the view using an exposed form.
- * - $sort_order: The select box with the ASC, DESC options to define order. Optional.
- * - $items_per_page: The select box with the available items per page. Optional.
+ * - $sort_order: Select box with ASC, DESC options to define order. Optional.
+ * - $items_per_page: Select box with the available items per page. Optional.
  * - $offset: A textfield to define the offset of the view. May be optional.
  * - $reset_button: A button to reset the exposed filter applied. Optional.
  * - $button: The submit button for the form.
