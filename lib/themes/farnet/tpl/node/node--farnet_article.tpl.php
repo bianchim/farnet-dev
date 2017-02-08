087df87dca0a0bf461f4a7dc6928c9e39cbfe16e
Sokarion <nicolas.lorant@loretic.com>
2017-02-08 15:31:44 +0000

    MULTISITE-15796-v2 Fix remarks

--- lib/themes/farnet/tpl/node/node--farnet_article.tpl.php
+++ lib/themes/farnet/tpl/node/node--farnet_article.tpl.php
@@ -8,9 +8,13 @@
 
 <div class="content clearfix">
 
-  <div class="media">
+  <div class="media media--farnet">
     <?php if (!empty($content['field_picture'])) : ?>
-      <?php print render($content['field_picture']); ?>
+      <div class="media-left">
+        <?php if (!empty($content['field_picture'])) : ?>
+          <?php print render($content['field_picture']); ?>
+        <?php endif; ?>
+      </div>
     <?php endif; ?>
     <div class="media-body">
       <?php if (!empty($content['field_ne_body'])) : ?>
@@ -19,31 +23,27 @@
     </div>
   </div>
 
-  <div id="group-farnet-article-information" class="group-farnet-article-information field-group-tab">
-    <h3 class="fr-heading"><span>Information</span></h3>
-    <table class="table table-responsive table-blue table--white-borders">
-      <tbody>
-      <?php if (!empty($content['field_term_country'])) : ?>
-        <tr>
-          <th scope="row"><?php print $content['field_term_country']['#title']; ?></th>
-          <td class="multi-values"><?php print render($content['field_term_country']); ?></td>
-        </tr>
-      <?php endif; ?>
-      <?php if (!empty($content['field_term_theme'])) : ?>
-        <tr>
-          <th scope="row"><?php print $content['field_term_theme']['#title']; ?></th>
-          <td class="multi-values"><?php print render($content['field_term_theme']); ?></td>
-        </tr>
-      <?php endif; ?>
-      <?php if (!empty($content['field_tags'])) : ?>
-        <tr>
-          <th scope="row"><?php print $content['field_tags']['#title']; ?></th>
-          <td class="multi-values"><?php print render($content['field_tags']); ?></td>
-        </tr>
-      <?php endif; ?>
-      </tbody>
-    </table>
-  </div>
+  <?php if ((!empty($content['field_term_country'])) || (!empty($content['field_term_theme']))) : ?>
+    <div id="group-farnet-article-information" class="group-farnet-article-information field-group-tab">
+      <h3 class="fr-heading"><span>Information</span></h3>
+      <table class="table table-responsive table-blue table--white-borders">
+        <tbody>
+        <?php if (!empty($content['field_term_country'])) : ?>
+          <tr>
+            <th scope="row"><?php print $content['field_term_country']['#title']; ?></th>
+            <td class="multi-values"><?php print render($content['field_term_country']); ?></td>
+          </tr>
+        <?php endif; ?>
+        <?php if (!empty($content['field_term_theme'])) : ?>
+          <tr>
+            <th scope="row"><?php print $content['field_term_theme']['#title']; ?></th>
+            <td class="multi-values"><?php print render($content['field_term_theme']); ?></td>
+          </tr>
+        <?php endif; ?>
+        </tbody>
+      </table>
+    </div>
+  <?php endif; ?>
 
   <div class="link-wrapper right"></div>
 
@@ -53,6 +53,8 @@
       <?php print render($content['field_page']); ?>
     </div>
   <?php endif; ?>
+
+  <div class="u-mt-1em"></div>
 
   <?php if (!empty($content['field_publication_date'])) : ?>
     <?php print render($content['field_publication_date']); ?>
