2d0f1393c90688635047b3f363d699f6c5a8c1b7
Sokarion <nicolas.lorant@loretic.com>
2017-02-13 15:06:56 +0000

    MULTISITE_15974-v2 Fix phpcs

--- lib/themes/farnet/tpl/node/node--factsheet-flag.tpl.php
+++ lib/themes/farnet/tpl/node/node--factsheet-flag.tpl.php
@@ -79,7 +79,6 @@
  */
 ?>
 
-<?php dpm($content); ?>
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
   <div class="content clearfix"<?php print $content_attributes; ?>>
