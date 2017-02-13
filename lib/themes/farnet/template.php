ca2b34079869992384b192a5f17e2d0ad926db9d
bianchim <matthias.bianchi@vesperiagroup.com>
2017-02-13 14:39:59 +0000

    Normalize display of 'sea basin' and 'type of area' MULTISITE-16157

--- lib/themes/farnet/template.php
+++ lib/themes/farnet/template.php
@@ -874,3 +874,34 @@ function farnet_item_list($variables) {
   }
   return $output;
 }
+
+/**
+ * Returns HTML for a date element formatted as a range.
+ */
+function farnet_date_display_range($variables) {
+  $date1 = $variables['date1'];
+  $date2 = $variables['date2'];
+  $timezone = $variables['timezone'];
+  $attributes_start = $variables['attributes_start'];
+  $attributes_end = $variables['attributes_end'];
+  $show_remaining_days = $variables['show_remaining_days'];
+
+  $start_date = '<span class="date-display-start"' . drupal_attributes($attributes_start) . '>' . $date1 . '</span>';
+  $end_date = '<span class="date-display-end"' . drupal_attributes($attributes_end) . '>' . $date2 . $timezone . '</span>';
+
+  // If microdata attributes for the start date property have been passed in,
+  // add the microdata in meta tags.
+  if (!empty($variables['add_microdata'])) {
+    $start_date .= '<meta' . drupal_attributes($variables['microdata']['value']['#attributes']) . '/>';
+    $end_date .= '<meta' . drupal_attributes($variables['microdata']['value2']['#attributes']) . '/>';
+  }
+
+  // Wrap the result with the attributes.
+  $output = '<div class="date-display-range">' . t('From !start-date to !end-date', array(
+    '!start-date' => $start_date,
+    '!end-date' => $end_date,
+  )) . '</div>';
+
+  // Add remaining message and return.
+  return $output . $show_remaining_days;
+}
