cb34473f0e0663c582b5d967ea912e83dd1b7320
Sokarion <nicolas.lorant@loretic.com>
2017-03-08 16:48:29 +0000

    MULTISITE-16778 Create Tpl, CT add permission, etc ...

--- lib/themes/farnet/tpl/node/node--myfarnet_event.tpl.php
+++ lib/themes/farnet/tpl/node/node--myfarnet_event.tpl.php
@@ -2,14 +2,14 @@
 
 /**
  * @file
- * Override of node.tpl.php for MyFarnet News.
+ * Override of node.tpl.php for MyFarnet Event.
  */
 ?>
 
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
   <div class="content clearfix"<?php print $content_attributes; ?>>
-    <?php if ($prefix_display):?>
+    <?php if ($prefix_display): ?>
       <div class="node-private label label-default clearfix">
         <span class="glyphicon glyphicon-lock"></span>
         <?php print t('This content is private'); ?>
@@ -18,7 +18,7 @@
 
     <div class="media media--farnet">
       <?php if (!empty($content['field_picture'])) : ?>
-        <div class="media-left ">
+        <div class="media-left">
           <?php print render($content['field_picture']); ?>
         </div>
       <?php endif; ?>
@@ -29,15 +29,32 @@
       </div>
     </div>
 
-    <?php if ((!empty($content['field_term_country'])) || (!empty($content['field_term_theme']))) : ?>
-    <div id="group-myfarnet-news-information" class="group-myfarnet-news-information field-group-tab">
+    <div id="group-myfarnet-event-information" class="group-myfarnet-event-information field-group-tab">
       <h3 class="fr-heading"><span>Information</span></h3>
       <table class="table table-responsive table-blue table--white-borders">
         <tbody>
+        <?php if (!empty($content['field_dates'])) : ?>
+          <tr>
+            <th scope="row"><?php print $content['field_dates']['#title']; ?></th>
+            <td class="multi-values"><?php print render($content['field_dates']); ?></td>
+          </tr>
+        <?php endif; ?>
         <?php if (!empty($content['field_term_country'])) : ?>
           <tr>
             <th scope="row"><?php print $content['field_term_country']['#title']; ?></th>
             <td class="multi-values"><?php print render($content['field_term_country']); ?></td>
+          </tr>
+        <?php endif; ?>
+        <?php if (!empty($content['field_city'])) : ?>
+          <tr>
+            <th scope="row"><?php print $content['field_city']['#title']; ?></th>
+            <td class="multi-values"><?php print render($content['field_city']); ?></td>
+          </tr>
+        <?php endif; ?>
+        <?php if (!empty($content['field_event_type'])) : ?>
+          <tr>
+            <th scope="row"><?php print $content['field_event_type']['#title']; ?></th>
+            <td class="multi-values"><?php print render($content['field_event_type']); ?></td>
           </tr>
         <?php endif; ?>
         <?php if (!empty($content['field_term_theme'])) : ?>
@@ -49,12 +66,11 @@
         </tbody>
       </table>
     </div>
-    <?php endif; ?>
 
     <div class="link-wrapper right"></div>
 
     <?php if (!empty($content['field_related_documents'])) : ?>
-      <div id="group-myfarnet-news-documents" class="group-myfarnet-news-documents field-group-tab">
+      <div id="group-myfarnet-event-documents" class="group-myfarnet-event-documents field-group-tab">
         <h3 class="fr-heading"><span><?php print $content['field_related_documents']['#title']; ?></span></h3>
         <?php print render($content['field_related_documents']); ?>
       </div>
@@ -65,4 +81,5 @@
     <?php print render($content['comments']); ?>
 
   </div>
+
 </div>
