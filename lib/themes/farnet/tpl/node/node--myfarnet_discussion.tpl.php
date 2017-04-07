b36253919befb89a9a256847d3ddd38fac39f9cd
Sokarion <nicolas.lorant@loretic.com>
2017-04-07 11:19:02 +0000

    FAR-762 Update CT MyFarnet Discussion

--- lib/themes/farnet/tpl/node/node--myfarnet_discussion.tpl.php
+++ lib/themes/farnet/tpl/node/node--myfarnet_discussion.tpl.php
@@ -38,9 +38,10 @@
         </div>
       <?php endif; ?>
     </div>
+
     <div class="media media--farnet">
       <?php if (!empty($content['field_picture'])) : ?>
-        <div class="media-left ">
+        <div class="media-left">
           <?php print render($content['field_picture']); ?>
         </div>
       <?php endif; ?>
@@ -51,12 +52,32 @@
       </div>
     </div>
 
+    <div id="group-myfarnet-discussion-information" class="group-myfarnet-discussion-information field-group-tab">
+      <h3 class="fr-heading"><span>Information</span></h3>
+      <table class="table table-responsive table-blue table--white-borders">
+        <tbody>
+        <?php if (!empty($content['field_term_country'])) : ?>
+          <tr>
+            <th scope="row"><?php print $content['field_term_country']['#title']; ?></th>
+            <td class="multi-values"><?php print render($content['field_term_country']); ?></td>
+          </tr>
+        <?php endif; ?>
+        <?php if (!empty($content['field_term_theme'])) : ?>
+          <tr>
+            <th scope="row"><?php print $content['field_term_theme']['#title']; ?></th>
+            <td class="multi-values"><?php print render($content['field_term_theme']); ?></td>
+          </tr>
+        <?php endif; ?>
+        </tbody>
+      </table>
+    </div>
+
     <div class="link-wrapper right"></div>
 
-    <?php if (!empty($content['field_fc_document'])) : ?>
+    <?php if (!empty($content['field_related_documents'])) : ?>
       <div id="group-myfarnet-discussion-documents" class="group-myfarnet-discussion-documents field-group-tab">
-        <h3 class="fr-heading"><span><?php print $content['field_fc_document']['#title']; ?></span></h3>
-        <?php print render($content['field_fc_document']); ?>
+        <h3 class="fr-heading"><span><?php print $content['field_related_documents']['#title']; ?></span></h3>
+        <?php print render($content['field_related_documents']); ?>
       </div>
     <?php endif; ?>
 
