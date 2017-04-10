94317a9596dcb642ca4622f4a3ac07261364259d
Sokarion <nicolas.lorant@loretic.com>
2017-04-10 22:46:47 +0000

    FAR-786 create tpl, modify view, fix phpcs

--- lib/themes/farnet/tpl/views/views-view-unformatted--farnet-communities-members.tpl.php
+++ lib/themes/farnet/tpl/views/views-view-unformatted--farnet-communities-members.tpl.php
@@ -2,17 +2,29 @@
 
 /**
  * @file
+ * Override of views-view-unformatted.tpl.php.
+ */
+?>
+<ul class="media-list farnet-listing">
+  <?php foreach ($rows as $id => $row): ?>
+    <?php print $row; ?>
+  <?php endforeach; ?>
+</ul>
+
+<?php
+
+/**
+ * @file
  * Default simple view template to display a list of rows.
  *
  * @ingroup views_templates
  */
 ?>
-
 <?php if (!empty($title)): ?>
   <h3><?php print $title; ?></h3>
 <?php endif; ?>
 <?php foreach ($rows as $id => $row): ?>
-  <div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .'"';  } ?>>
+  <div<?php if ($classes_array[$id]) {print ' class="' . $classes_array[$id] . '"';} ?>>
     <?php print $row; ?>
   </div>
 <?php endforeach; ?>
