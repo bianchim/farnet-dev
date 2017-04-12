632671165eb58174cb36cc49ae13517eb41f3c9e
bianchim <matthias.bianchi@vesperiagroup.com>
2017-04-12 08:36:26 +0000

    Change myfarnet button handling

--- lib/themes/farnet/tpl/om-maximenu-submenu-links.tpl.php
+++ lib/themes/farnet/tpl/om-maximenu-submenu-links.tpl.php
@@ -7,12 +7,7 @@
 ?>  
 
 <?php if (!empty($permission)): ?>
-  <?php
-    $myfarnet = $content['link_title'] == 'myFARNET' ? 'c-myfarnet-btn' : '';
-    if ($myfarnet !== '' && $myfarnet_active) {
-      $myfarnet .= ' active';
-    }
-  ?>
+  <?php $myfarnet = $content['link_title'] == 'myFARNET' ? 'c-myfarnet-btn' : ''; ?>
   <li id="om-leaf-<?php print $code . '-' . $key; ?>" class="<?php print om_maximenu_link_classes($content, $permission, $count, $total); ?> navigation-main-item <?php echo $myfarnet; ?>">
     <?php print $om_link; ?>
     <?php
