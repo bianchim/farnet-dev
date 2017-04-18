3408a1cf78b37eb66f83eeb6b0c0948d39ea27f6
ccontet <ccontet@github.com>
2017-04-18 15:03:54 +0000

    feature/FAR-808 - Subscription button on myFarnet CT added

--- lib/themes/farnet/tpl/node/node--myfarnet_cooperation_idea.tpl.php
+++ lib/themes/farnet/tpl/node/node--myfarnet_cooperation_idea.tpl.php
@@ -101,7 +101,7 @@
     <?php endif; ?>
 
     <?php print render($content['links']); ?>
-
+    <?php print render($content['subscriptions_ui']); ?>
     <?php print render($content['comments']); ?>
 
   </div>
