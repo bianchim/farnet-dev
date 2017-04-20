a4db09ccc6c32fc75e0931267e601724b51f7463
bianchim <matthias.bianchi@vesperiagroup.com>
2017-04-20 09:13:58 +0000

    Fix path for contact me form

--- lib/themes/farnet/tpl/user/user-profile.tpl.php
+++ lib/themes/farnet/tpl/user/user-profile.tpl.php
@@ -98,7 +98,7 @@
     <div class="col-md-6">
       <div class="contacts">
         <div class="user-profile-block-contact">
-          <a href="/user/<?php echo $user_id; ?>/contact" type="message" class="btn btn-info btn-block">
+          <a href="<?php echo base_path(); ?>user/<?php echo $user_id; ?>/contact" type="message" class="btn btn-info btn-block">
             <span class="glyphicon glyphicon-envelope"></span>
             Contact me</a>
         </div>
